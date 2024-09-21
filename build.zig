b92d9fcdbe884e99de183ce035c232d146d475c7
Ratakor <ratakor@disroot.org>
2024-09-21 15:06:23 +0200

    Add linenoise

--- build.zig
+++ build.zig
@@ -4,12 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const linenoise = buildLinenoise(b, target, optimize);
     const exe = b.addExecutable(.{
         .name = "nov",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe.linkLibC();
+    exe.linkLibrary(linenoise);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -20,31 +23,30 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const release = b.step("release", "Make an upstream binary release");
-    const release_targets: []const std.Target.Query = &.{
-        .{ .cpu_arch = .aarch64, .os_tag = .macos },
-        .{ .cpu_arch = .aarch64, .os_tag = .linux },
-        .{ .cpu_arch = .x86_64, .os_tag = .linux },
-        .{ .cpu_arch = .x86_64, .os_tag = .windows },
-    };
-    for (release_targets) |target_query| {
-        const rel_exe = b.addExecutable(.{
-            .name = "nov",
-            .root_source_file = b.path("src/main.zig"),
-            .target = b.resolveTargetQuery(target_query),
-            .optimize = .ReleaseSafe,
-            .strip = true,
-        });
+    // const release = b.step("release", "Make an upstream binary release");
+    // const release_targets: []const std.Target.Query = &.{
+    //     .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    //     .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    //     .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    //     .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    // };
+    // for (release_targets) |target_query| {
+    //     const rel_exe = b.addExecutable(.{
+    //         .name = "nov",
+    //         .root_source_file = b.path("src/main.zig"),
+    //         .target = b.resolveTargetQuery(target_query),
+    //         .optimize = .ReleaseSafe,
+    //         .strip = true,
+    //     });
+    //     const install = b.addInstallArtifact(rel_exe, .{});
+    //     install.dest_dir = .prefix;
+    //     install.dest_sub_path = b.fmt("{s}-{s}", .{
+    //         target_query.zigTriple(b.allocator) catch unreachable,
+    //         rel_exe.name,
+    //     });
 
-        const install = b.addInstallArtifact(rel_exe, .{});
-        install.dest_dir = .prefix;
-        install.dest_sub_path = b.fmt("{s}-{s}", .{
-            target_query.zigTriple(b.allocator) catch unreachable,
-            rel_exe.name,
-        });
-
-        release.dependOn(&install.step);
-    }
+    //     release.dependOn(&install.step);
+    // }
 
     const fmt_step = b.step("fmt", "Format all source files");
     fmt_step.dependOn(&b.addFmt(.{ .paths = &.{ "build.zig", "src" } }).step);
@@ -52,4 +54,24 @@ pub fn build(b: *std.Build) void {
     const clean_step = b.step("clean", "Remove build artifacts");
     clean_step.dependOn(&b.addRemoveDirTree(".zig-cache").step);
     clean_step.dependOn(&b.addRemoveDirTree("zig-out").step);
+}
+
+fn buildLinenoise(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) *std.Build.Step.Compile {
+    const lib = b.addStaticLibrary(.{
+        .name = "linenoise",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.addIncludePath(b.path("vendor/linenoise"));
+    lib.addCSourceFile(.{
+        .file = b.path("vendor/linenoise/linenoise.c"),
+        .flags = &[_][]const u8{ "-Os" },
+    });
+    lib.linkLibC();
+    b.installArtifact(lib);
+    return lib;
 }
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const Scanner = @import("Scanner.zig");
 const Chunk = @import("Chunk.zig");
 const VM = @import("vm.zig").VM;
 const debug = @import("debug.zig");
+const ln = @import("linenoise.zig");
 
 pub fn main() !void {
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
@@ -11,27 +12,6 @@ pub fn main() !void {
 
     var vm = VM.init(allocator);
     defer vm.deinit();
-
-    // var chunk = Chunk.init(allocator);
-    // defer chunk.deinit();
-    // var constant = try chunk.addConstant(1.2);
-    // try chunk.write(@intFromEnum(Chunk.OpCode.constant), 123);
-    // try chunk.write(@intCast(constant), 123);
-
-    // constant = try chunk.addConstant(3.4);
-    // try chunk.write(@intFromEnum(Chunk.OpCode.constant), 123);
-    // try chunk.write(@intCast(constant), 123);
-
-    // try chunk.write(@intFromEnum(Chunk.OpCode.add), 123);
-
-    // constant = try chunk.addConstant(5.6);
-    // try chunk.write(@intFromEnum(Chunk.OpCode.constant), 123);
-    // try chunk.write(@intCast(constant), 123);
-    // try chunk.write(@intFromEnum(Chunk.OpCode.negation), 123);
-
-    // try chunk.write(@intFromEnum(Chunk.OpCode.@"return"), 123);
-    // debug.disassembleChunk(&chunk, "test chunk");
-    // try vm.interpret(&chunk);
 
     var args = try std.process.argsWithAllocator(allocator);
     defer args.deinit();
@@ -60,47 +40,25 @@ fn runFile(allocator: std.mem.Allocator, vm: *VM, path: []const u8) !void {
 }
 
 fn repl(allocator: std.mem.Allocator, vm: *VM) !void {
-    const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
-    var array_list = try std.ArrayList(u8).initCapacity(allocator, 4096);
-    defer array_list.deinit();
+    const history_path = if (std.posix.getenv("XDG_STATE_HOME")) |xdg_state|
+        try std.fmt.allocPrintZ(allocator, "{s}/nov-history", .{xdg_state})
+    else if (std.posix.getenv("HOME")) |home|
+        try std.fmt.allocPrintZ(allocator, "{s}/.local/state/nov-history", .{home})
+    else
+        try allocator.dupeZ(u8, "./nov-history");
+    defer allocator.free(history_path);
+
+    _ = ln.linenoiseHistorySetMaxLen(1000);
+    _ = ln.linenoiseHistoryLoad(history_path);
 
     while (true) {
-        try stdout.writeAll("> ");
-        stdin.streamUntilDelimiter(array_list.writer(), '\n', null) catch |err| switch (err) {
-            error.EndOfStream => if (array_list.items.len == 0) {
-                try stdout.writeAll("\n");
-                break;
-            },
-            else => return err,
-        };
-        try array_list.append(0);
-        const bytes = array_list.items[0 .. array_list.items.len - 1 :0];
-        vm.interpret(allocator, bytes) catch {};
-        array_list.clearRetainingCapacity();
+        if (ln.linenoise("> ")) |bytes| {
+            const line = std.mem.span(bytes);
+            vm.interpret(allocator, line) catch continue;
+            _ = ln.linenoiseHistoryAdd(bytes);
+            _ = ln.linenoiseHistorySave(history_path);
+        } else {
+            break;
+        }
     }
 }
-
-// fn run(allocator: std.mem.Allocator, source: [:0]const u8) !void {
-//     _ = allocator;
-//     var scanner = Scanner.init(source);
-//     const stdout = std.io.getStdOut().writer();
-//     while (true) {
-//         const token = scanner.next();
-//         try stdout.print("{}\n", .{token});
-//         if (token.tag == .eof) {
-//             break;
-//         }
-//     }
-// }
-
-// pub fn err(line: usize, comptime fmt: []const u8, args: anytype) void {
-//     report(line, "", fmt, args);
-// }
-
-// fn report(line: usize, where: []const u8, comptime fmt: []const u8, args: anytype) void {
-//     const stderr = std.io.getStdErr().writer();
-//     stderr.print("[line {d}] Error {s}: ", .{ line, where }) catch {};
-//     stderr.print(fmt, args) catch {};
-//     stderr.writeAll("\n") catch {};
-// }
