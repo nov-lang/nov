90f91672569ae5b7e19a71e2a5d0f49adf854bbc
Ratakor <ratakor@disroot.org>
2024-09-26 19:19:11 +0200

    Add old Parser and Ast

--- src/Codegen.zig
+++ src/Codegen.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const Ast = @import("Ast.zig");
 const Node = Ast.Node;
 const _value = @import("value.zig");
+const Value = _value.Value;
+const Chunk = @import("Chunk.zig");
 const isPrimitive = @import("primitives.zig").isPrimitive;
 
 const Codegen = @This();
@@ -9,6 +11,7 @@ const Codegen = @This();
 ast: *const Ast,
 allocator: std.mem.Allocator,
 arena: std.mem.Allocator,
+chunk: Chunk,
 
 pub fn generate(allocator: std.mem.Allocator, ast: Ast) !void {
     var arena = std.heap.ArenaAllocator.init(allocator);
@@ -18,6 +21,7 @@ pub fn generate(allocator: std.mem.Allocator, ast: Ast) !void {
         .allocator = allocator,
         .arena = arena.allocator(),
         .ast = &ast,
+        .chunk = Chunk.init(allocator),
     };
 
     try codegen.generateNode(0);
@@ -25,7 +29,8 @@ pub fn generate(allocator: std.mem.Allocator, ast: Ast) !void {
 
 const Nodegen = *const fn (*Codegen, Node.Index) anyerror!void;
 const generators = std.enums.directEnumArray(Node.Tag, Nodegen, 0, .{
-    // .block = generateBlock,
+    // .negation = generateNegation,
+    .int_literal = generateIntLiteral,
 });
 
 fn generateNode(self: *Codegen, node: Node.Index) !void {
@@ -34,4 +39,36 @@ fn generateNode(self: *Codegen, node: Node.Index) !void {
     if (generators[@intFromEnum(self.ast.nodes.items(.tag)[node])]) |gen| {
         return gen(self, node);
     }
+}
+
+fn emit(self: *Codegen, code: u32) !void {
+    return self.chunk.write(code, self.previous.line);
+}
+
+fn emitOpCode(self: *Codegen, code: Chunk.OpCode) !void {
+    return self.emit(@intFromEnum(code));
+}
+
+fn emitCodeArg(self: *Codegen, code: Chunk.OpCode, arg: u24) !void {
+    return self.emit(@as(u32, @intFromEnum(code)) | (@as(u32, @intCast(arg)) << 8));
+}
+
+fn makeConstant(self: *Codegen, value: Value) !u24 {
+    return self.chunk.addConstant(value);
+}
+
+fn emitConstant(self: *Codegen, value: Value) !void {
+    return self.emitCodeArg(.constant, self.makeConstant(value));
+}
+
+// fn generateNegation(self: *Codegen, node: Node.Index) !void {
+//     const operand = self.ast.nodes.items(.negation)[node];
+//     try self.generateNode(operand);
+//     try self.chunk.write(Chunk.OpCode.negation, self.ast.nodes.items(.line)[node]);
+// }
+
+fn generateIntLiteral(self: *Codegen, node: Node.Index) !void {
+    const literal = self.ast.nodes.items(.main_token)[node];
+    const value = Value.create(try std.fmt.parseInt(i64, literal, 0));
+    try self.emitConstant(value);
 }
--- src/test.zig
+++ src/test.zig
@@ -4,7 +4,7 @@ const Node = Ast.Node;
 
 pub fn main() !void {
     // (* (- 123) (group 45.67))
-    // const source = "-123 * (45.67)\n";
+    const source = "-123 * (45.67)\n";
     // const source =
     //     // \\let x: int = 3
     //     // \\let mut y = "salut"
@@ -20,82 +20,77 @@ pub fn main() !void {
     //     \\x + y
     //     \\
     // ;
-    const source =
-        \\let cond = true
-        \\if (cond) {
-        \\    let mut x = "outer"
-        \\    {
-        \\        x = (x + " ") * 3
-        \\        x *= 2
-        \\    }
-        \\    print(x)
-        \\} else {
-        \\    print("Hello world!")
-        \\}
-        \\
-        \\let mut i: int = 0
-        \\loop {
-        \\    print(i)
-        \\    i += 1
-        \\    if (i == 10) {
-        \\        break
-        \\    }
-        \\}
-        \\
-    ;
+    // const source =
+    //     \\let cond = true
+    //     \\if (cond) {
+    //     \\    let mut x = "outer"
+    //     \\    {
+    //     \\        x = (x + " ") * 3
+    //     \\        x *= 2
+    //     \\    }
+    //     \\    print(x)
+    //     \\} else {
+    //     \\    print("Hello world!")
+    //     \\}
+    //     \\
+    //     \\let mut i: int = 0
+    //     \\loop {
+    //     \\    print(i)
+    //     \\    i += 1
+    //     \\    if (i == 10) {
+    //     \\        break
+    //     \\    }
+    //     \\}
+    //     \\
+    // ;
 
-    std.log.debug("Running parser with source: \n{s}\n", .{source});
+    // std.log.debug("Running parser with source: \n{s}\n", .{source});
 
     const allocator = std.heap.page_allocator;
     var ast = try Ast.parse(allocator, source);
     defer ast.deinit(allocator);
 
+    // for (ast.rootDecls()) |decl| {
+    //     // std.debug.print("{} {}\n", .{decl, ast.nodes.items(.tag)[decl]});
+    //     for (ast.firstToken(decl)..ast.lastToken(decl) + 1) |token| {
+    //         std.debug.print("{s} ", .{ast.tokenSlice(@intCast(token))});
+    //     }
+    //     std.debug.print("\n", .{});
+    // }
+
     // std.debug.print("Tokens:", .{});
     // for (ast.tokens.items(.tag)) |tag| {
     //     std.debug.print(" {s}", .{@tagName(tag)});
     // }
-    std.debug.print("\nNodes:", .{});
-    for (ast.nodes.items(.tag)) |tag| {
-        std.debug.print(" {s}", .{@tagName(tag)});
-    }
-    std.debug.print("\nErrors:", .{});
-    for (ast.errors) |parse_error| {
-        std.debug.print("\n", .{});
-        const loc = ast.tokenLocation(0, parse_error.token);
-        std.debug.print("{d}:{d}: {s}: ", .{
-            loc.line,
-            loc.column,
-            @tagName(ast.tokens.items(.tag)[parse_error.token]),
-        });
-        try ast.renderError(parse_error, std.io.getStdOut().writer());
-    }
-    std.debug.print("\n", .{});
 
-    _ = ast.firstToken(0);
-    _ = ast.lastToken(0);
+    // std.debug.print("\nNodes:", .{});
+    // for (ast.nodes.items(.tag)) |tag| {
+    //     std.debug.print(" {s}", .{@tagName(tag)});
+    // }
+    // std.debug.print("\nErrors:", .{});
+    // for (ast.errors) |parse_error| {
+    //     std.debug.print("\n", .{});
+    //     const loc = ast.tokenLocation(0, parse_error.token);
+    //     std.debug.print("{d}:{d}: {s}: ", .{
+    //         loc.line,
+    //         loc.column,
+    //         @tagName(ast.tokens.items(.tag)[parse_error.token]),
+    //     });
+    //     try ast.renderError(parse_error, std.io.getStdOut().writer());
+    // }
+    // std.debug.print("\n", .{});
+
+    // _ = ast.firstToken(0);
+    // _ = ast.lastToken(0);
 
     // const main_tokens = ast.nodes.items(.main_token);
     // const node_tags = ast.nodes.items(.tag);
     // const datas = ast.nodes.items(.data);
     // std.debug.print("{} {} {} {} {} {} {}\n", .{ main_tokens[1], datas[2].lhs, node_tags[datas[2].lhs], datas[5].lhs, node_tags[datas[5].lhs], datas[0].lhs, node_tags[datas[0].lhs] });
 
-    var buffer = std.ArrayList(u8).init(allocator);
-    defer buffer.deinit();
-    try @import("render.zig").renderTree(&buffer, ast, .{});
-    std.debug.print("{s}\n", .{buffer.items});
-
-    // ast.rootDecls();
-    // print(&ast, 0, std.io.getStdOut().writer());
+    // var buffer = std.ArrayList(u8).init(allocator);
+    // defer buffer.deinit();
+    // try @import("render.zig").renderTree(&buffer, ast, .{});
+    // std.debug.print("{s}\n", .{buffer.items});
 
 }
-
-// fn print(self: *Ast, index: Node.Index, writer: anytype) void {
-//     const node = self.nodes.get(index);
-//     if (node.data.lhs != 0) {
-//         print(self, node.data.lhs, writer);
-//     }
-//     writer.writeAll(self.tokens.get(node.main_token).tag.symbol()) catch unreachable;
-//     if (node.data.rhs != 0) {
-//         print(self, node.data.rhs, writer);
-//     }
-// }
